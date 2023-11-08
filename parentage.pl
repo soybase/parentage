35be5d64a81b5eedca04151dc04d9abb414c8710
StevenCannon-USDA <steven.cannon@ars.usda.gov>
2023-11-08 10:18:18 -0600

    Add flag -noself to remove the starting ID from the parentage string.

--- parentage.pl
+++ parentage.pl
@@ -12,6 +12,7 @@ use Getopt::Long;
 my $parents;
 my $start;
 my $format = "string,table";
+my $noself;
 my $verbose;
 my $help;
 
@@ -38,6 +39,7 @@ my $usage = <<EOS;
     -start    ID of an individual for which to calculate parentage.
               If not provided, report parentage for all individuals.
     -format   Output format. Options: string, table [string,table]
+    -noself   Remove the starting ID from the parentage string.
     -verbose  Report some intermediate information.
     -help     This message.
 EOS
@@ -46,6 +48,7 @@ GetOptions(
   'parents=s'  => \$parents,  # required
   'start:s'    => \$start,
   'format:s'   => \$format,
+  'noself'     => \$noself,
   'v|verbose'  => \$verbose,
   'h|help'     => \$help,
 );
@@ -81,11 +84,7 @@ if ($start){ # Starting individual was provided, so calculate parentage for it
   $START_IND = $ind;
   $ped_str = "[$ind]: < $ind >";
   ped($ind, \%HoA);
-  if ($format =~ /string/){ 
-    $ped_str =~ s/</(/g; $ped_str =~ s/>/)/g; 
-    $ped_str =~ s/\[([^]]+)\]:/$1:/;
-    say $ped_str, "\n" 
-  }
+  if ($format =~ /string/){ print_ped_string($ped_str) }
 }
 else { # No starting individual was provided, so calculate parentage for all
   while( my ($key, $value) = each(%HoA)) {
@@ -93,16 +92,12 @@ else { # No starting individual was provided, so calculate parentage for all
     $START_IND = $ind;
     $ped_str = "[$ind]: < $ind >";
     ped($ind, \%HoA);
-    if ($format =~ /string/){ 
-      $ped_str =~ s/</(/g; $ped_str =~ s/>/)/g; 
-      $ped_str =~ s/\[([^]]+)\]:/$1:/;
-      say $ped_str 
-    }
+    if ($format =~ /string/){ print_ped_string($ped_str) }
     if ($format =~ /table/){ say "" }
   }
 }
 
-sub ped{
+sub ped {
   my $key = shift;
   my $hshref = shift;
   for my $item ($hshref){
@@ -111,26 +106,51 @@ sub ped{
       $p1 = $item->{$key}->[0];
       $p2 = $item->{$key}->[1];
       if ( defined($p1) && defined($p2) ){
-        if ($format =~ /table/){ say join("\t", $START_IND, $key, $p1, $p2) }
+        if ($format =~ /table/){ print_table_row($START_IND, $key, $p1, $p2) }
         $ped_str =~ s/ $key / $key < $p1 , $p2 > /g;
         ped($p1, $hshref);
         ped($p2, $hshref);
       }
       elsif ( defined($p1) && !defined($p2) ){
-        if ($format =~ /table/){ say join("\t", $START_IND, $key, $p1, "-") }
+        if ($format =~ /table/){ print_table_row($START_IND, $key, $p1, "-") }
         $ped_str =~ s/ $key / $key < $p1 > /g;
         ped($p1, $hshref);
       }
       elsif ( !defined($p1) && defined($p2) ){
-        if ($format =~ /table/){ say join("\t", $START_IND, $key, "-", $p2) }
+        if ($format =~ /table/){ print_table_row($START_IND, $key, "-", $p2) }
         $ped_str =~ s/ $key / $key < $p2 > /g;
         ped($p2, $hshref);
       }
     }
     else { # Individual is without a parent, so return
-      if ($format =~ /table/){ say join("\t", $START_IND, $key, "-", "-") }
+      if ($format =~ /table/){ print_table_row($START_IND, $key, "-", "-") }
       return 0;
     }
+  }
+}
+
+sub print_ped_string {
+  my $ped_str = shift;
+  if ($noself){ # If set, remove starting ID prior to printing
+    $ped_str =~ s/^(\S+:) < \S+ (<.+ >) >$/$1 $2/;
+  }
+  $ped_str =~ s/</(/g; $ped_str =~ s/>/)/g; 
+  $ped_str =~ s/\[([^]]+)\]:/$1:/;
+  say "$ped_str";
+}
+
+sub print_table_row {
+  my $st_ind = shift; my $key = shift; my $p1 = shift; my $p2 = shift;
+  if ($noself){ # Don't print line that contains the individual and its parents
+    if ($key eq $START_IND){
+      return;
+    }
+    else {
+      say join("\t", $st_ind, $key, $p1, $p2);
+    }
+  }
+  else {
+    say join("\t", $st_ind, $key, $p1, $p2);
   }
 }
 
@@ -139,3 +159,5 @@ __END__
 Versions
 2023-11-06 Initial version
 2023-11-07 Report starting individual in first column of table output
+2023-11-08 Add flag -noself to remove the starting ID from the parentage string.
+
